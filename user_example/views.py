94d7ce329b842a6c86f2311b946afedb6f4a1410
Yvon Huynh <yvon.huynh@gmail.com>
2020-12-22 21:57:33 +0100

    add user, redirect OK

--- user_example/views.py
+++ user_example/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate,login
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def register(request):
-    form = UserCreationForm()
-    context = {'form': form}
-    return render(request, 'registration/register.html',context)
+    if request.method == 'POST':
+        form  = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, pasword=password)
+            login(request,user)
+            return redirect('index')
+    else:
+
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, 'registration/register.html',context)
