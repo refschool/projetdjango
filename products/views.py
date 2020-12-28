7085b4f6e6bd4a260dd0f27736faf7c065914f9b
Yvon Huynh <yvon.huynh@gmail.com>
2020-12-28 20:23:51 +0100

    need to add request.FILES plus instance=product to make it not create a new product instead of updating existing product

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render
 from .models import Product
-
 from .forms import NameForm, ProductForm,MyProductForm
 
 # authentication tutorial
@@ -19,30 +18,23 @@ def index(request):
     print('***products')
     print(products)
     return render(request, 'index.html', { 'products': products })
-
+#authentication
 #https://www.youtube.com/watch?v=qwE9TFNub84
 
 def liste(request):
     products = Product.objects.all()
-    print('***products')
-    print(products)
     return render(request, 'liste.html', { 'products': products })
 
 
-def add_product_form(request):
+def add_product(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = MyProductForm(request.POST, request.FILES)
-        print('in the add 1a ******')
-        print(request.POST)
-        print('in the add 1b ******')
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            print('in the add 2 ******')
             product = form.save(commit=False)
-            print('*********',request.FILES)
             product.save()
             # redirect to a new URL:
             return HttpResponseRedirect('/products/thanks')
@@ -54,31 +46,26 @@ def add_product_form(request):
     return render(request, 'myproductform.html', {'form': form})
 
 def edit_product(request,pk):
+    #https://www.youtube.com/watch?v=EX6Tt-ZW0so&t=264s
+    product = Product.objects.get(id=pk)
+    form = MyProductForm(instance=product)
+
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = MyProductForm(request.POST)
-
+        form = MyProductForm(request.POST,request.FILES,instance=product)
         # check whether it's valid:
         if form.is_valid():
             # TODO process the data in form.cleaned_data as required
-            product = form.save()
-
+            print('form is valid')
+            product = form.save(commit=False)
+            product.save()
             # commit=False tells Django that "Don't send this to database yet.
             # I have more things I want to do with it."
             # name = form.cleaned_data['name']
             # save https://www.youtube.com/watch?v=dBctY3-Z5hY
             # redirect to a new URL:
             return HttpResponseRedirect('/products/thanks')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        print('my_recordxxx ',pk)
-        my_record = Product.objects.get(id=pk)
-        print('start printing my_recordxxx',my_record.name)
-        print('****')
-        form = MyProductForm(instance=my_record)
-
     return render(request, 'editform.html', {'form': form})
 
 def thanks(request):
